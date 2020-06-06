7c9a81321e09e8e38831534724003980940f3c17
juliomchado <juliocarlos00@hotmail.com>
2020-06-06 20:05:35 +0100

    Estrutura da pagina Detail finalizada

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Feather as Icon } from '@expo/vector-icons'
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
-import { View, StyleSheet, TouchableOpacity, Image, Text } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native'
 import Constants from 'expo-constants'
-
+import { RectButton } from 'react-native-gesture-handler'
 
 const Detail = () => {
 
@@ -15,15 +15,38 @@ const Detail = () => {
 
 
     return (
-        <View style={styles.container}>
-            <TouchableOpacity onPress={handleNavigateBack}>
-                <Icon name="arrow-left" size={20} color="#34cb79" />
-            </TouchableOpacity>
+        <SafeAreaView style={{ flex: 1 }}>
 
-            <Image  style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1580913428023-02c695666d61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }}/>
-        
-            <Text style={styles.pointName}>Mercadão do João</Text>
-        </View>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={handleNavigateBack}>
+                    <Icon name="arrow-left" size={20} color="#34cb79" />
+                </TouchableOpacity>
+
+                <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1580913428023-02c695666d61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }} />
+
+                <Text style={styles.pointName}>Mercadão do João</Text>
+                <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+
+                <View style={styles.address}>
+                    <Text style={styles.addressTitle}>Endereço</Text>
+                    <Text style={styles.addressContent}>Rio do Sul, SC</Text>
+
+                </View>
+
+            </View>
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={() => { }}>
+                    <FontAwesome name="whatsapp" size={20} color="#fff" />
+                    <Text style={styles.buttonText}>Whatsapp</Text>
+                </RectButton>
+
+                <RectButton style={styles.button} onPress={() => { }}>
+                    <Icon name="mail" size={20} color="#fff" />
+                    <Text style={styles.buttonText}>E-mail</Text>
+                </RectButton>
+            </View>
+        </SafeAreaView>
+
 
 
 
@@ -34,7 +57,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 32,
-        paddingTop: 20,
+        paddingTop: 40,
     },
 
     pointImage: {
