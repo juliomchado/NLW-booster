ef919724f2fc3d34ca00fc2c891ff8170cd69da6
juliomchado <juliocarlos00@hotmail.com>
2020-06-12 00:55:21 +0100

    Erro de syntax nas rotas do backend consertada

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,53 +1,41 @@
-import express from 'express'
+  
+import express from 'express';
 import { celebrate, Joi } from 'celebrate';
 
-import multer from 'multer'
-import multerConfig from './config/multer'
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-import PointsController from './controllers/PointsController'
-import ItemsController from './controllers/ItemsController'
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
-const routes = express.Router()
+const routes = express.Router(); 
 const upload = multer(multerConfig);
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-routes.get('/items', itemsController.index)
+routes.get('/items', itemsController.index);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', upload.single('image'),
-    celebrate({
-        body: Joi.object().keys({
-            name: Joi.string().required(),
-            email: Joi.string().required().email(),
-            whatsapp: Joi.number().required(),
-            latitude: Joi.number().required(),
-            longitude: Joi.number().required(),
-            city: Joi.string().required(),
-            uf: Joi.string().required().max(2),
-            items: Joi.string().required()
-        }),{
-            abortEarly: false
-        }
-    }),
-    pointsController.create)
-
-routes.get('/points', pointsController.index)
-routes.get('/points/:id', pointsController.show)
-
-
+routes.post(
+  '/points', 
+  upload.single('image'), 
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false
+  }),
+  pointsController.create
+);
 
 export default routes;
-
-// { 
-// "name": "Mercado do seu Zé", 
-// "email": "contato@imperatriz.com.br", 
-// "whatsapp": "462131312313",
-// "latitude": 37.421976, 
-// "longitude": -122.0840161, 
-// "city": "Rio do Sul",
-// "uf": "SC", 
-// "items": [
-//     6
-// ]
-// }
